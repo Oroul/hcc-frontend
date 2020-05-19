895491616ecc29e8fc82090c7f6a23249233a94a
Austen Bouza <austen.bouza@gmail.com>
2020-05-19 13:18:40 -0400

    Project Setup
    Includes first mock of metric boxes with index values.

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="app">
+    <div id="metrics">
+      <div id="totalScore">
+        {{ totalScore }}
+      </div>
+      <div v-if="selectedMetric === undefined" class="flex">
+        <MetricBox
+          v-for="metric in metrics"
+          @select-metric="selectMetric(metric)"
+          :metric="metric"
+          :key="metric.name"
+        />
+      </div>
+      <div v-else>
+        <div id="metricPane">
+          <MetricBox
+            :metric="selectedMetric"
+            @select-metric="selectMetric(selectedMetric)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MetricBox from '@/components/MetricBox'
+
+export default {
+  name: 'App',
+  components: {
+    MetricBox
+  },
+  methods: {
+    selectMetric(metric) {
+      console.log('selectMetric')
+      this.selectedMetric = (this.selectedMetric === undefined ? metric : undefined)
+    }
+  },
+  data () {
+    return {
+      totalScore: 80,
+      selectedMetric: undefined,
+      metrics: [
+        {
+          'name': 'Health-Adjusted Life Expectancy',
+          'indexScore': 58.5,
+          'slope': 0.1
+        },
+        {
+          'name': 'Median Household Income',
+          'indexScore': 76.0,
+          'slope': -0.2
+        },
+        {
+          'name': 'Mental Health',
+          'indexScore': 58.5,
+          'slope': 0.1
+        },
+        {
+          'name': 'Childhood Success Rate',
+          'indexScore': 46.0,
+          'slope': 0
+        },
+        {
+          'name': 'Social and Economic Mobility',
+          'indexScore': 88.5,
+          'slope': 0.1
+        },
+        {
+          'name': 'Absence of Substance Abuse',
+          'indexScore': 36.0,
+          'slope': -0.2
+        },
+        {
+          'name': 'Metric #7',
+          'indexScore': 53.3,
+          'slope': 0.1
+        },
+        {
+          'name': 'Metric #8',
+          'indexScore': 76.0,
+          'slope': -0.2
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#metrics {
+  border: 1px dotted black;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+#totalScore {
+  text-align: center;
+  font-size: xxx-large;
+  border: 1px dotted black;
+}
+
+#metricPane {
+  border: 1px dotted black;
+}
+
+.flex {
+  display: flex;
+}
+</style>
---
+++ src/components/MetricBox.vue
@@ -0,0 +1,71 @@
+<template>
+  <div @click="$emit('select-metric')" id="metricContainer">
+    <div id="metricBox" :class="slopeType(metric.slope)">
+      {{ metric.indexScore }}
+    </div>
+    <div id="metricName">
+      {{ metric.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      metric: Object
+    },
+    methods: {
+      slopeType: (slope) => {
+        if (slope > 0) {
+          return 'increasing'
+        } else if (slope < 0) {
+          return 'decreasing'
+        } else {
+          return 'constant'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#metricContainer {
+  width: 10em;
+  border: 1px solid black;
+  margin: 0.5em;
+  background: #5fa6c6;
+}
+
+#metricContainer:hover {
+  cursor: pointer;
+}
+
+#metricBox {
+  display: flex;
+  height: 5em;
+  justify-content: center;
+  align-items: center;
+  font-size: xx-large;
+  border-bottom: 1px solid black;
+  color: white;
+}
+
+#metricName {
+  color: white;
+  text-align: center;
+  padding-top: 0.2em;
+  padding-bottom: 0.2em;
+}
+
+.increasing {
+  background: green;
+}
+
+.decreasing {
+  background: red;
+}
+
+.constant {
+  background: lightgrey;
+}
+</style>
